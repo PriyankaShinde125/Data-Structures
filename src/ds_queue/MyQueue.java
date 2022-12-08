5b279ca70ef98554fa86b7429e3f1d55966c7be6
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-09 00:12:10 +0530

    ADDED : Code to perform all queue operations

--- src/ds_queue/MyQueue.java
+++ src/ds_queue/MyQueue.java
@@ -5,15 +5,29 @@ import ds_linkedlist.MyLinkedList;
 
 public class MyQueue<T> {
     MyLinkedList<T> myQueue;
+
     public MyQueue() {
-        myQueue =new MyLinkedList<>();
+        myQueue = new MyLinkedList<>();
     }
+
     public void enqueue(INode<T> element) {
         myQueue.append(element);
     }
 
     @Override
     public String toString() {
-        return  myQueue.toString();
+        return myQueue.toString();
+    }
+
+    public INode<T> dequeue() {
+        return myQueue.popFirst();
+    }
+
+    public boolean isEmpty() {
+        return myQueue.isEmpty();
+    }
+
+    public int size() {
+        return myQueue.size();
     }
 }
--- src/ds_queue/MyQueueMain.java
+++ src/ds_queue/MyQueueMain.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 public class MyQueueMain {
     private static final int ENQUEUE = 1;
     private static final int EXIT = 0;
+    private static final int DEQUEUE = 2;
+    private static final int IS_EMPTY = 3;
+    private static final int SIZE = 4;
     MyQueue<Integer> myQueue;
 
     public static void main(String[] args) {
@@ -15,7 +18,7 @@ public class MyQueueMain {
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("Enter your choice");
-            System.out.println("1 : Enqueue \n0 : Exit");
+            System.out.println("1 : Enqueue \n2 : Dequeue \n3 : IsEmpty \n4 : Size \n0 : Exit");
             int choice = sc.nextInt();
             switch (choice) {
                 case ENQUEUE:
@@ -23,9 +26,18 @@ public class MyQueueMain {
                     myQueueMain.myQueue.enqueue(new MyNode<>(sc.nextInt()));
                     System.out.println(myQueueMain.myQueue);
                     break;
+                case DEQUEUE:
+                    System.out.println(myQueueMain.myQueue.dequeue());
+                    System.out.println(myQueueMain.myQueue);
+                    break;
+                case IS_EMPTY:
+                    System.out.println(myQueueMain.myQueue.isEmpty() ? "Queue is empty " : "Queue is not empty");
+                    break;
+                case SIZE:
+                    System.out.println(myQueueMain.myQueue.size());
+                    break;
                 case EXIT:
                     return;
-
             }
         }
     }
